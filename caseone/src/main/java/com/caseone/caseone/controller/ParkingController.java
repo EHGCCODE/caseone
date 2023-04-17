a095ff54d12522563d8ade9861ecba4425ae36b2
EHGCCODE <hannover.tano@gmail.com>
2023-04-17 18:30:13 -0500

    creacion de clases getAllCars y addCar

--- caseone/src/main/java/com/caseone/caseone/controller/ParkingController.java
+++ caseone/src/main/java/com/caseone/caseone/controller/ParkingController.java
@@ -1,4 +1,36 @@
 package com.caseone.caseone.controller;
 
 public class ParkingController {
+    private String licenseLate;
+    private String color;
+    private String entryTrue;
+
+    public ParkingController(String licenseLate, String color) {
+        this.licenseLate = licenseLate;
+        this.color = color;
+    }
+
+    public String getLicenseLate() {
+        return licenseLate;
+    }
+
+    public void setLicenseLate(String licenseLate) {
+        this.licenseLate = licenseLate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getEntryTrue() {
+        return entryTrue;
+    }
+
+    public void setEntryTrue(String entryTrue) {
+        this.entryTrue = entryTrue;
+    }
 }
--- caseone/src/main/java/com/caseone/caseone/services/ParkingService.java
+++ caseone/src/main/java/com/caseone/caseone/services/ParkingService.java
@@ -1,4 +1,23 @@
 package com.caseone.caseone.services;
 
+import java.util.*;
+import com.caseone.caseone.entities.Car;
 public class ParkingService {
+    private List<Car> parkingLot;
+
+    public ParkingService() {
+        this.parkingLot = new ArrayList<>();
+    }
+    //TODO:Como usuario, quiero poder ver la lista de todos los autos estacionados en el
+    //parqueo, para poder tener una visión general del estado actual del
+    //estacionamiento.
+    public List<Car> getAllCars(){
+        return parkingLot;
+    }
+    // TODO:omo usuario, quiero poder buscar un auto por su placa, para poder encontrar
+    //rápidamente un vehículo en particular.
+    public void addCar(Car car){
+        this.parkingLot.add(car);
+    }
+
 }
